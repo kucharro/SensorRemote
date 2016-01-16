332f5668322a9cab21037da73a8835b65ca94d9c
kucharro <kucharek.kamil@gmail.com>
2016-01-16 19:13:10 +0100

    working

--- app/src/main/java/com/example/kucharro/sensorremote/MainActivity.java
+++ app/src/main/java/com/example/kucharro/sensorremote/MainActivity.java
@@ -8,24 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        initListeners();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void initListeners() {
+        button = (Button) findViewById(R.id.HelloButton);
+        button.setOnClickListener(this);
+
     }
 
     @Override
@@ -48,5 +48,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.HelloButton)
+            Toast.makeText(this,"Yay, you clicked!", Toast.LENGTH_SHORT).show();
     }
 }
